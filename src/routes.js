8e24991d19ffe875d368448fc458c4b4f8a6dba8
francine1919 <francine1919@gmail.com>
2022-03-15 12:53:26 -0300

    Add editar perfil

--- src/routes.js
+++ src/routes.js
@@ -1,32 +1,30 @@
-import React from 'react';
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
-import Address from './pages/Address';
-import Cart from './pages/Cart';
-import Feed from './pages/Feed';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Profile from './pages/Profile';
-import Search from './pages/Search';
-import Signup from './pages/SignUp';
-
+import React from "react";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Address from "./pages/Address";
+import Cart from "./pages/Cart";
+import Feed from "./pages/Feed";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Search from "./pages/Search";
+import Signup from "./pages/SignUp";
 
 const RoutesApp = () => {
-
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element ={<Home />} />
-                <Route path="/address" element ={<Address />} />
-                <Route path="/cart" element ={<Cart />} />
-                <Route path="/feed" element ={<Feed />} />
-                <Route path="/login" element ={<Login />} />
-                <Route path="/profile" element ={<Profile />} />
-                <Route path="/search" element ={<Search />} />
-                <Route path="/signup" element ={<Signup />} />
-            </Routes>
-        </BrowserRouter> 
-    );
-    
-}
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/address" element={<Address />} />
+        <Route path="/feed" element={<Feed />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/editar" element={<Editar />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default RoutesApp;
