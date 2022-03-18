afd602bc9432e3290561629798c379133262001a
Arthut Kelvim <kelvimarthur@gmail.com>
2022-03-18 17:00:08 -0300

    Correção do botão de excluir do carrinho

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -1,33 +1,45 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import GlobalState from "../../Global/GlobalState";
 import { Container } from "./styled";
 import { GlobalContext } from "../../Global/GlobalContext";
+import useRemoveItem from "../../hook/useRemoveItem";
 
 const Address = () => {
   const { cart } = useContext(GlobalContext);
+  const removeDoCarrinho = useRemoveItem()
+  
   // Recenbendo o cart do local storage
   const retrievedCart = localStorage.getItem("cart");
   const cartItems = JSON.parse(retrievedCart);
+  console.log(cart)
+    const renderiza = () => {
+      
+      const listaCarrinho = cart?.map((pedido) => {
+        return (
+          <div>
+            <p>{pedido.name}</p>
+            <p>R${pedido.preco}</p>
+            <p>Quantidade:{pedido.quantidade}</p>
+            <button onClick={() => removeDoCarrinho(pedido)}>Remover</button>
+            <hr
+              style={{
+                background: "#274837",
+                height: 5,
+              }}
+            />
+          </div>
+        );
+      });
+      return listaCarrinho
+    }
+    useEffect(() => {
+      renderiza()
+    }, [cartItems])
+  
 
-  const listaCarrinho = cartItems?.map((pedido) => {
-    return (
-      <div>
-        <p>{pedido.name}</p>
-        <p>R${pedido.preco}</p>
-        <p>Quantidade:{pedido.quantidade}</p>
-        <hr
-          style={{
-            background: "#274837",
-            height: 5,
-          }}
-        />
-      </div>
-    );
-  });
-  console.log(cart);
   return (
     <div>
-      <Container>{listaCarrinho}</Container>
+       <Container>{renderiza()}</Container> 
     </div>
   );
 };
