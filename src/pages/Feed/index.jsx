fc174c785789a8061dc188cbdd4bcd85b6f2097e
francine1919 <francine1919@gmail.com>
2022-03-22 01:01:07 -0300

    update feed

--- src/pages/Feed/index.jsx
+++ src/pages/Feed/index.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 import { HeaderPage } from "../../components/Header/HeaderPage";
 import useProtectedPage from "../../hook/useProtectedPage ";
 import useForm from "../../hook/useForm";
-import { Img, Main, Container, Description, Inputs, FormSearch, Time, Category, Data } from './styled'
+import { Img, Main, Container, Description, Inputs, FormSearch, Time, Category, Data, Select } from './styled'
 
 const Feed = () => {
   useProtectedPage();
@@ -55,15 +55,13 @@ const Feed = () => {
       .filter((rest) => {
         return rest.category
           .toLowerCase()
-          .replace("á", "a")
+          //.replace("á", "a")
           .includes(form.category.toLowerCase());
       })
       .map(({ id, name, category, address, deliveryTime, logoUrl }) => {
         return (
           <Main key={id}>
-            <button onClick={() => navigate(`/menu/${id}`)}>
-              <Img src={logoUrl} />
-            </button>
+            <Img src={logoUrl} onClick={() => navigate(`/menu/${id}`)} />
             <h2>{name}</h2>
             <p>{category}</p>
             <p>{deliveryTime} min</p>
@@ -72,25 +70,30 @@ const Feed = () => {
         );
       });
   return (
-    <div>
+    <>
       <HeaderPage />
-      <FormSearch onSubmit={pesquisar}>
-        <Inputs
-          name="pesquisar"
-          value={form.pesquisar}
-          onChange={onChangeForm}
-          placeholder={"Pesquisar por nome"}
-        />
-        <Inputs
-          name="category"
-          value={form.category}
-          onChange={onChangeForm}
-          placeholder={"Pesquisar por categoria"}
-        />
-      </FormSearch>
-      {filterRests()}
-      {/* {restaurante ? <p>Restaurante não encontrado.</p> : filterRests()} */}
-    </div>
+      <Container>
+        <FormSearch onSubmit={pesquisar}>
+          <Inputs
+            name="pesquisar"
+            value={form.pesquisar}
+            onChange={onChangeForm}
+            placeholder={"Pesquisar por nome"}
+          />
+          <Select name="category" value={form.category} onChange={onChangeForm}>
+            <option value={""} disabled>
+              Cozinhas
+            </option>
+            <option value={""}>Todas as categorias</option>
+            {restaurants.map(({ category }) => {
+              return <option>{category}</option>;
+            })}
+          </Select>
+        </FormSearch>
+        {filterRests()}
+        {/* {restaurante ? <p>Restaurante não encontrado.</p> : filterRests()} */}
+      </Container>
+    </>
   );
 };
 
